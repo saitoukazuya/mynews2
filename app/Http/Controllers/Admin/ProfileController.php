e8f0516154cb413b550b45aff3a98f50b8eb94dd
saitoukazuya <qz.14.kz@gmail.com>
2020-05-08 08:38:29 +0000

    5/8

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+}
