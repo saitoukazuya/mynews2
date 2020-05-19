332104c36a4f049f2803a549f2232cbfcda1ec3d
saitoukazuya <qz.14.kz@gmail.com>
2020-05-19 13:11:28 +0000

    5/19_6

--- database/migrations/2020_05_19_055858_create_profiles_histories_table.php
+++ database/migrations/2020_05_19_055858_create_profiles_histories_table.php
@@ -13,7 +13,7 @@ class CreateProfilesHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('profile_histories', function (Blueprint $table) {
+        Schema::create('profiles_histories', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('profile_id');
             $table->string('edited_at');
